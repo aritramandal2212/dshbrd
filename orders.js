8cfc2ecb9d9813fb588a014309663ebeb5803712
aritramandal2212 <88551604+aritramandal2212@users.noreply.github.com>
2022-08-10 13:01:38 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName:'Ruko Fu Pro Drone',
+        productNumber:'49347',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Drone with Camera',
+        productNumber:'96996',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'GPS 4K Drone',
+        productNumber:'22821',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'DJI Air 2S',
+        productNumber:'81475',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Lozenge Drone',
+        productNumber:'00482',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    }
+]
